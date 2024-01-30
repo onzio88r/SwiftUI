c4be5779141a2862d159061794cf3213573d779b
Daniele Rapali <daniele.rapali@gmail.com>
2024-01-30 12:04:45 +0100

    Create NotificationView.swift
    In app custom notification view banner

---
+++ NotificationView.swift
@@ -0,0 +1,90 @@
+//
+//  NotificationView.swift
+//  Atom
+//
+//  Created by Daniele Rapali - Technogym on 30/01/24.
+//  Copyright © 2024 Igloocy srl. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            ZStack {
+                    HStack {
+                        Image(systemName: "app.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(.leading, 8)
+                            .padding(.trailing, 7)
+                        
+                        Text("App Name")
+                            .font(
+                                Font.custom("SF Pro Text", size: 12)
+                                    .weight(.medium)
+                            )
+                            .foregroundColor(.white.opacity(0.2))
+                        
+                        Spacer()
+                        Text("1h ago")
+                            .font(
+                                Font.custom("SF Pro Text", size: 12)
+                                    .weight(.medium)
+                            )
+                            .multilineTextAlignment(.trailing)
+                            .foregroundColor(.white.opacity(0.2))
+                            .padding(.trailing, 16)
+                        
+                    }
+                    .frame(maxWidth: .infinity, minHeight: 20, maxHeight: 20)
+                    
+                    
+                
+            }
+            .frame(maxWidth: .infinity, minHeight: 20, maxHeight: 20)
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {   Text("Title")
+                        .font(
+                            Font.custom("SF Pro Text", size: 14)
+                                .weight(.bold)
+                        )
+                        .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white.opacity(0.5))
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                        }
+                .padding(.horizontal, 12)
+                .padding(.vertical, 0)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                
+              
+            }
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {   Text("Subtitle")
+                        .font(
+                            Font.custom("SF Pro Text", size: 14)
+                                .weight(.light)
+                        )
+                        .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white.opacity(0.5))
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                        }
+                .padding(.horizontal, 12)
+                .padding(.vertical, 0)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                
+              
+            }
+        }
+        .padding(.horizontal, 0)
+        .padding(.vertical, 10)
+        .background(.white.opacity(0))
+        .background(.black.opacity(0.45))
+        .cornerRadius(12)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
